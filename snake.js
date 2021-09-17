fe7b650411dfb6323778d10ce8ed97ba1ddbee21
Rubalpreet kaur <84393565+Rubalpreet832@users.noreply.github.com>
2021-09-17 08:45:31 -0700

    Add files via upload

---
+++ snake.js
@@ -0,0 +1,23 @@
+
+    window.requestAnimationFrame(main);
+window.addEventListener('keydown',e=>{
+    inputDir ={x:0,y:01}
+    moveSound.play();
+    switch(e.key){
+        case "ArrowUp":
+            console.log("ArrowUp")
+            break;
+        
+     case "ArrowDown":
+    console.log("ArrowDown")
+    break;
+    case "ArrowLeft":
+        console.log("ArrowLeft")
+        break;
+        case "ArrowRight":
+            console.log("ArrowRight")
+            break;
+     default:
+         break;   
+    }
+})
